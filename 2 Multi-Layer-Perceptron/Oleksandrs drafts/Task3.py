ed2ef835be094c42f12d96469aee192e254e9c32
developsomethingcool <pometunalexander@gmail.com>
2022-11-09 19:03:36 +0100

    Addided tasks3,4

---
+++ 2 Multi-Layer-Perceptron/Oleksandrs drafts/Task3.py
@@ -0,0 +1,13 @@
+import Task02
+
+class MLP():
+    def __init__(self, n_hidden_layers, n_units):
+        self.n_hidden_layers = n_hidden_layers
+        self.n_units = n_units
+        self.layer = Task02.Layer(n_units, ) [for i in range(n_hidden_layers)]
+
+    def forward_step(self):
+        pass
+
+    def baclpropagantion(self):
+        pass
---
+++ 2 Multi-Layer-Perceptron/Oleksandrs drafts/Task4.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def MSE(y, t):
+    return 1/2 * (y-t) ** 2
+
+
